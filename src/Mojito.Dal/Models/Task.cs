9c965f858d83a3240b70b038a96ce7d00cae261d
Jon Zajac <jon.zajac@relianta.com>
2014-04-01 22:00:43 -0400

    Need to decorate PK with AutoIncrement to properly create table.

--- src/Mojito.Dal/Models/Task.cs
+++ src/Mojito.Dal/Models/Task.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServiceStack.DataAnnotations;
 
 namespace Mojito.Dal.Models
 {
     public class Task : IBaseEntity
     {
+        [AutoIncrement]
         public int Id { get; set; }
 
         public string Name { get; set; }
