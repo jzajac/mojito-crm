2791d0bcc6159a31a12694037264431fd76920a5
Jon Zajac <jon.zajac@relianta.com>
2014-04-01 21:55:47 -0400

    Updating API to integrate with new repository and service for Tasks

---
+++ src/Mojito.API/TaskModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Mojito.API
+{
+    using Nancy;
+
+    public class TaskModule : NancyModule
+    {
+        public TaskModule() : base("/task")
+        {
+            Get["/"] = parameters =>
+            {
+
+                var taskService = new BusinessLogic.Services.TaskService();
+
+                return Negotiate
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithContentType("application/json")
+                    .WithModel(taskService.FindAll().ToArray());
+            };
+        }
+    }
+}
