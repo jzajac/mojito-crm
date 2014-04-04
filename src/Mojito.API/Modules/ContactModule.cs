54663b242287fa0755198e656804e0498447e92f
Jon Zajac <jon.zajac@relianta.com>
2014-04-04 07:27:09 -0400

    Moving Nancy modules into modules directory. Adding Contact module.

---
+++ src/Mojito.API/Modules/ContactModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mojito.API
+{
+    using Nancy;
+
+    public class ContactModule : NancyModule
+    {
+        public ContactModule()
+            : base("/contact")
+        {
+            Get["/"] = parameters =>
+            {
+
+                var contactService = new BusinessLogic.Services.ContactService();
+
+                return Negotiate
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithContentType("application/json")
+                    .WithModel(contactService.FindAll().ToArray());
+            };
+        }
+    }
+}
